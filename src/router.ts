2f2ceb0929563b5749d814785778e87af08e1887
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-23 19:29:30 +0530

    Add router for API endpoints

--- src/router.ts
+++ src/router.ts
@@ -22,3 +22,5 @@ router.get("/updatepoint/:id", () => {});
 router.put("/updatepoint/:id", () => {});
 router.post("/updatepoint", () => {});
 router.delete("/updatepoint/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hi Mom");
 });
+app.use("/api", router);
 
 export default app;
