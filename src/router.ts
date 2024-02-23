e7f6b5280eba046f69dbe9420f0bcf0b0d483e02
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-23 19:16:23 +0530

    Add routes for update functionality

--- src/router.ts
+++ src/router.ts
@@ -6,3 +6,11 @@ router.get("/product/:id", () => {});
 router.put("/product/:id", () => {});
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
+
+// Update
+
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
