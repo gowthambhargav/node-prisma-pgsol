789631b27d9cafe8f42978af4f2b47eeff32b5c7
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-23 19:17:28 +0530

    Add update point routes

--- src/router.ts
+++ src/router.ts
@@ -14,3 +14,11 @@ router.get("/update/:id", () => {});
 router.put("/update/:id", () => {});
 router.post("/update", () => {});
 router.delete("/update/:id", () => {});
+
+// Update Points
+
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.delete("/updatepoint/:id", () => {});
