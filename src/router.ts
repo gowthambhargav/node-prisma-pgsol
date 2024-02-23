5b1cd1a6488f498849933a349afb78938f8085ea
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-23 19:15:22 +0530

    Add new files and update existing files

---
+++ src/router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
