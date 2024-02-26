4d333dc6ffea2aaa39868a31a89adaedcee650b2
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-26 18:11:53 +0530

    Add environment-specific configuration files***

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from "./server";
+import config from "./config";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-app.listen(3000, () => {
-  console.log("server is listening");
+app.listen(config.port, () => {
+  console.log("server is listening", config.port);
 });
